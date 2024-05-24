3e1c20c0d73328df93a5a4cf269c8d0c2ea15d4f
Johann284 <johann_ruth@estudante.sesisenai.org.br>
2024-05-24 11:43:33 -0300

    novo: corpo base das funções para trocar página do cadastro para a página seguinte

--- js/cadastro.js
+++ js/cadastro.js
@@ -13,10 +13,47 @@ function passarCadastro() {
 
 */
 
-new paginas = ['cadastroUm.html', 'cadastroDois.html', 'cadastroTres.html', 'cadastroQuatro.html', 'cadastroCinco.html']
 
+let permissao = false;
 
+function checarPermissoes() {
+    switch (document.URL){
+        case document.URL.includes("cadastroUm.html"):
+            checarPermissaoTelaUm();
+            break;
+        case document.URL.includes("cadastroDois.html"):
+            checarPermissaoTelaDois();
+            break;
+        case document.URL.includes("cadastroTres.html"):
+            checarPermissaoTelaTres();
+            break;
+        case document.URL.includes("cadastroQuatro.html"):
+            checarPermissaoTelaQuatro();
+            break;
+        case document.URL.includes("cadastroCinco.html"):
+            checarPermissaoTelaCinco();
+            break;
+    }
+}
 
-function passarPagina() {
-
+function passarPagina() { //função para passar de página
+    if  (checarPermissoes()){  
+        switch (document.URL){
+            case document.URL.includes("cadastroUm.html"):
+                window.location.href = "cadastroDois.html";
+                break;
+            case document.URL.includes("cadastroDois.html"):
+                window.location.href = "cadastroTres.html";
+                break;
+            case document.URL.includes("cadastroTres.html"):
+                window.location.href = "cadastroQuatro.html";
+                break;
+            case document.URL.includes("cadastroQuatro.html"):
+                window.location.href = "cadastroCinco.html";
+                break;
+            case document.URL.includes("cadastroCinco.html"):
+                window.location.href = "login.html";
+                break;
+        }
+    } //caso ele tiver permissão (os valores do cadastro estão válidos, ele troca para a página seguinte)
 }
