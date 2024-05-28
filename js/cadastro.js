5ba6803a6d8776a3714dd1572adefcf815213546
Lucas-Candinho <lucas_candinho1@estudante.sesisenai.org.br>
2024-05-28 11:23:49 -0300

    Criação da passagem do CadastroUm para CadastroDois

--- js/cadastro.js
+++ js/cadastro.js
@@ -16,57 +16,19 @@ function passarCadastro() {
 
 // TO DO:  imprementar BD
 
-function checarPermissaoTelaUm(){  //funcao para identificar valores HTML e garantir que estão preenchidos
-    var nome = document.getElementById("input-nome").textContent;
-    var idade = document.getElementById("input-idade").textContent;
-    if (idade < 0 || idade > 120) {
-        return false;
-    }
-    if (nome.length() <= 0) {
-        return false;
-    }
-    return true;
+function checarTelaUm(){  //funcao para identificar valores HTML e garantir que estão preenchidos
+    let nome = document.getElementById("input-nome").value;
+    let idade = document.getElementById("input-idade").value;
+
+    return idade > 0 && idade < 120 && nome != "";
 }
-
-function checarPermissaoTelaDois(){ console.log("TODO")};
-function checarPermissaoTelaTres(){ console.log("TODO")};
-function checarPermissaoTelaQuatro(){ console.log("TODO")};
-function checarPermissaoTelaCinco(){ console.log("TODO")};
-
 
 function checarPermissoes() { // função para identificar tela para armazenar dados
-    switch (document.URL){
-        case document.URL.includes("cadastroUm.html"):
-            return checarPermissaoTelaUm();
-        case document.URL.includes("cadastroDois.html"):
-            return checarPermissaoTelaDois();
-        case document.URL.includes("cadastroTres.html"):
-            return checarPermissaoTelaTres();
-        case document.URL.includes("cadastroQuatro.html"):
-            return checarPermissaoTelaQuatro();
-        case document.URL.includes("cadastroCinco.html"):
-            return checarPermissaoTelaCinco();
-    } 
+    if (window.location.href.includes("cadastroUm.html")) { return checarTelaUm() };
 }
 
+function gerarErro() { console.log("GERANDO ERRO") }
+
 function passarPagina() { //função para passar de página 
-    if  (checarPermissoes()){  
-        switch (document.URL){
-            case document.URL.includes("cadastroUm.html"):
-                window.location.href = "cadastroDois.html";
-                break;
-            case document.URL.includes("cadastroDois.html"):
-                window.location.href = "cadastroTres.html";
-                break;
-            case document.URL.includes("cadastroTres.html"):
-                window.location.href = "cadastroQuatro.html";
-                break;
-            case document.URL.includes("cadastroQuatro.html"):
-                window.location.href = "cadastroCinco.html";
-                break;
-            case document.URL.includes("cadastroCinco.html"):
-                window.location.href = "login.html";
-                break;
-        }
-    } //caso ele tiver permissão (os valores do cadastro estão válidos, ele troca para a página seguinte)
+    window.location.href.includes("cadastroUm.html") && checarPermissoes() ? window.location.href = "cadastroDois.html" : gerarErro();
 }
