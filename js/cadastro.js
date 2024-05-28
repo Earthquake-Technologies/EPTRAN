902dbbc9479a8000fe04d3ae24ff4371d35c0599
Lucas-Candinho <lucas_candinho1@estudante.sesisenai.org.br>
2024-05-28 12:03:09 -0300

    Inicio da checagem da segunda tela

--- js/cadastro.js
+++ js/cadastro.js
@@ -23,12 +23,51 @@ function checarTelaUm(){  //funcao para identificar valores HTML e garantir que 
     return idade > 0 && idade < 120 && nome != "";
 }
 
+function checarTelaDois() {
+    const estadosDoBrasil = [
+        "Acre",
+        "Alagoas",
+        "Amapá",
+        "Amazonas",
+        "Bahia",
+        "Ceará",
+        "Distrito Federal",
+        "Espirito Santo",
+        "Goiás",
+        "Maranhão",
+        "Mato Grosso do Sul",
+        "Mato Grosso",
+        "Minas Gerais",
+        "Pará",
+        "Paraíba",
+        "Paraná",
+        "Pernambuco",
+        "Piauí",
+        "Rio de Janeiro",
+        "Rio Grande do Norte",
+        "Rio Grande do Sul",
+        "Rondônia",
+        "Roraima",
+        "Santa Catarina",
+        "São Paulo",
+        "Sergipe",
+        "Tocantins",
+        ];
+
+    let estado = document.getElementById("input-estado").value;
+    let cidade = document.getElementById("input-cidade").value;
+
+    return estadosDoBrasil.includes(estado) && cidade != "";
+}
+
 function checarPermissoes() { // função para identificar tela para armazenar dados
     if (window.location.href.includes("cadastroUm.html")) { return checarTelaUm() };
+    if (window.location.href.includes("cadastroDois.html")) { return checarTelaDois() };
 }
 
 function gerarErro() { console.log("GERANDO ERRO") }
 
 function passarPagina() { //função para passar de página 
     window.location.href.includes("cadastroUm.html") && checarPermissoes() ? window.location.href = "cadastroDois.html" : gerarErro();
+    window.location.href.includes("cadastroDois.html") && checarPermissoes() ? window.location.href = "cadastroTres.html" : gerarErro();
 }
