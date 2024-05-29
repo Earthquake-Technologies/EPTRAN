c7bd414bf4016c993fcb6538d2a39671ecf06d60
nicolasds3 <nicolas_veiga@estudante.sesisenai.org.br>
2024-05-29 12:50:46 -0300

    No html: importei duas fontes novas(Ropa Sans e Roboto Serif), também criei as informações que irão aparecer ao clicar. CSS: Criei os ids e classes para as infromações. No JS: Chamei as repectivas variáveis do html na parte de informações e fiz para ao clicar, aparecer as informações e desaparecer o titulo.

--- js/categorias.js
+++ js/categorias.js
@@ -1,3 +1,8 @@
 function mostrarInformacoes() {
-    document.getElementById("p-alterado").style.display = "none";
+    let p = document.getElementById("p-alterado");
+    p.style.display = "none";
+    let p1 = document.getElementById("p-alterado1");
+    p1.style.display = "none";
+    let info = document.getElementById("info-efi");
+    info.style.display = "block";
 }
