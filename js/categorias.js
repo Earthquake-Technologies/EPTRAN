b3a7e2af231186122274957509a7066fd2dddff7
nicolasds3 <nicolas_veiga@estudante.sesisenai.org.br>
2024-06-03 12:48:48 -0300

    No html: criei as informações de cada seção, explicando com comentários. No css: Criei novas classes e id's, para criar os padrões de layout's das seções. No js: Criei as funções para mostrar as informações de cada seção, e desaparecendo outras informações, tudo comentado e explicado no código.

--- js/categorias.js
+++ js/categorias.js
@@ -1,8 +1,69 @@
-function mostrarInformacoes() {
-    let p = document.getElementById("p-alterado");
-    p.style.display = "none";
-    let p1 = document.getElementById("p-alterado1");
+// Função que mostra as informações do "Ensino Fundamental I"
+function mostrarEFI() {
+    let p0 = document.getElementById("p0-alterado-EFI");
+    p0.style.display = "none";
+    let p1 = document.getElementById("p1-alterado-EFI");
     p1.style.display = "none";
-    let info = document.getElementById("info-efi");
-    info.style.display = "block";
+    let infoFundamental1 = document.getElementById("info-efi");
+    infoFundamental1.style.display = "block";
+    // Desaparecendo as informações do "Ensino Fundamental II"
+    let p2 = document.getElementById("p2-alterado-EFII");
+    p2.style.display = "";
+    let p3 = document.getElementById("p3-alterado-EFII");
+    p3.style.display = "";
+    let infoFundamental2 = document.getElementById("info-efii");
+    infoFundamental2.style.display = "none";
+    // Desaparecendo as informações do "Ensino Médio"
+    let p4 = document.getElementById("p4-alterado-EM");
+    p4.style.display = "";
+    let p5 = document.getElementById("p5-alterado-EM");
+    p5.style.display = "";
+    let infoMedio = document.getElementById("info-em");
+    infoMedio.style.display = "none";
+}
+// Função que mostra as informações do "Ensino Fundamental II"
+function mostrarEFII() {
+    let p2 = document.getElementById("p2-alterado-EFII");
+    p2.style.display = "none";
+    let p3 = document.getElementById("p3-alterado-EFII");
+    p3.style.display = "none";
+    let infoFundamental2 = document.getElementById("info-efii");
+    infoFundamental2.style.display = "block";
+    // Desaparecendo as informações do "Ensino Fundamental I"
+    let p0 = document.getElementById("p0-alterado-EFI");
+    p0.style.display = "";
+    let p1 = document.getElementById("p1-alterado-EFI");
+    p1.style.display = "";
+    let infoFundamental1 = document.getElementById("info-efi");
+    infoFundamental1.style.display = "none"; 
+    // Desaparecendo as informações do "Ensino Médio"
+    let p4 = document.getElementById("p4-alterado-EM");
+    p4.style.display = "";
+    let p5 = document.getElementById("p5-alterado-EM");
+    p5.style.display = "";
+    let infoMedio = document.getElementById("info-em");
+    infoMedio.style.display = "none";
+}
+// Função que mostra as informações do "Ensino Médio"
+function mostrarEM() {
+    let p4 = document.getElementById("p4-alterado-EM");
+    p4.style.display = "none";
+    let p5 = document.getElementById("p5-alterado-EM");
+    p5.style.display = "none";
+    let infoMedio = document.getElementById("info-em");
+    infoMedio.style.display = "block";
+    // Desaparecendo as informações do "Ensino Fundamental II"
+    let p2 = document.getElementById("p2-alterado-EFII");
+    p2.style.display = "";
+    let p3 = document.getElementById("p3-alterado-EFII");
+    p3.style.display = "";
+    let infoFundamental2 = document.getElementById("info-efii");
+    infoFundamental2.style.display = "none";
+    // Desaparecendo as informações do "Ensino Fundamental I"
+    let p0 = document.getElementById("p0-alterado-EFI");
+    p0.style.display = "";
+    let p1 = document.getElementById("p1-alterado-EFI");
+    p1.style.display = "";
+    let infoFundamental1 = document.getElementById("info-efi");
+    infoFundamental1.style.display = "none"; 
 }
