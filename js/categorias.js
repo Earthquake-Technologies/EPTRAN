9d7f0ba93299b4bbf91cd032342e6d945c9c9962
nicolasds3 <nicolas_veiga@estudante.sesisenai.org.br>
2024-06-11 12:51:02 -0300

    coloquei interrogação categorias funcional

--- js/categorias.js
+++ js/categorias.js
@@ -67,3 +67,48 @@ function mostrarEM() {
     let infoFundamental1 = document.getElementById("info-efi");
     infoFundamental1.style.display = "none"; 
 }
+
+var click = false;
+
+function infoTotal() {
+    click = !click;
+    if (click === true) {
+        let p0 = document.getElementById("p0-alterado-EFI");
+        p0.style.display = "none";
+        let p1 = document.getElementById("p1-alterado-EFI");
+        p1.style.display = "none";
+        let p2 = document.getElementById("p2-alterado-EFII");
+        p2.style.display = "none";
+        let p3 = document.getElementById("p3-alterado-EFII");
+        p3.style.display = "none";
+        let p4 = document.getElementById("p4-alterado-EM");
+        p4.style.display = "none";
+        let p5 = document.getElementById("p5-alterado-EM");
+        p5.style.display = "none";
+        let infoInterrogacaoEFI = document.getElementById("info-interrogacao-EFI");
+        infoInterrogacaoEFI.style.display = "block";
+        let infoInterrogacaoEFII = document.getElementById("info-interrogacao-EFII");
+        infoInterrogacaoEFII.style.display = "block";
+        let infoInterrogacaoEM = document.getElementById("info-interrogacao-EM");
+        infoInterrogacaoEM.style.display = "block";
+    } else {
+        let p0 = document.getElementById("p0-alterado-EFI");
+        p0.style.display = "";
+        let p1 = document.getElementById("p1-alterado-EFI");
+        p1.style.display = "";
+        let p2 = document.getElementById("p2-alterado-EFII");
+        p2.style.display = "";
+        let p3 = document.getElementById("p3-alterado-EFII");
+        p3.style.display = "";
+        let p4 = document.getElementById("p4-alterado-EM");
+        p4.style.display = "";
+        let p5 = document.getElementById("p5-alterado-EM");
+        p5.style.display = "";
+        let infoInterrogacaoEFI = document.getElementById("info-interrogacao-EFI");
+        infoInterrogacaoEFI.style.display = "none";
+        let infoInterrogacaoEFII = document.getElementById("info-interrogacao-EFII");
+        infoInterrogacaoEFII.style.display = "none";
+        let infoInterrogacaoEM = document.getElementById("info-interrogacao-EM");
+        infoInterrogacaoEM.style.display = "none";
+    }
+}
