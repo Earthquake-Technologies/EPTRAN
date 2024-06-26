6a6d8b34e22414e29b5b048839fd4d9e5a2aca80
nicolasds3 <nicolas_veiga@estudante.sesisenai.org.br>
2024-06-26 12:46:35 -0300

    Inserindo links no menu e correção ortográfica

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,12 @@ function avalienos(){
     window.location.href = "https://play.google.com/store/apps/details?id=br.gov.sc.joinville.facil.twa&hl=pt_BR&gl=US&pli=1";
 }
 
+function quemSomos(){
+    window.location.href = "quemSomos.html"
+}
+
 function atividades(){
-    window.location.href = "aprender.html";
+    window.location.href = "categorias.html";
 }
 
 function sair(){
